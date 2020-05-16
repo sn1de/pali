64002103b729d9c0e3fffee22a9656230fd43758
Brad Schneider <sn1de@users.noreply.github.com>
2020-05-16 14:30:54 -0500

    added button to trigger palindrome test

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 let Phrase = require("sn1de-palindrome");
-let string = prompt("Please enter a string for palindrome testing:");
-let phrase = new Phrase(string);
 
-if (phrase.palindrome()) {
-	alert(`"${phrase.content}" is a palindrome!`);
-} else {
-	alert(`"${phrase.content}" is not a palindrome.`);
+function palindromeTester() {
+	let string = prompt("Please enter a string for palindrome testing:");
+	let phrase = new Phrase(string);
+
+	if (phrase.palindrome()) {
+		alert(`"${phrase.content}" is a palindrome!`);
+	} else {
+		alert(`"${phrase.content}" is not a palindrome.`);
+	}
 }
+
+
+document.addEventListener("DOMContentLoaded", function() {
+	let button = document.querySelector("#palindromeTester");
+	button.addEventListener("click", function() {
+		palindromeTester();
+	});
+});
