8e4f5e023dae7633eb62e17cfbeb5b559f19731a
Brad Schneider <sn1de@users.noreply.github.com>
2020-05-16 14:38:38 -0500

    changed over to form from lone button

--- bundle.js
+++ bundle.js
@@ -14,8 +14,8 @@ function palindromeTester() {
 
 
 document.addEventListener("DOMContentLoaded", function() {
-	let button = document.querySelector("#palindromeTester");
-	button.addEventListener("click", function() {
+	let form = document.querySelector("#palindromeTester");
+	form.addEventListener("submit", function() {
 		palindromeTester();
 	});
 });
