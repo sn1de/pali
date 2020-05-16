1b00a9f76848f2c86ca057ec192e0cbfeeec3f56
Brad Schneider <sn1de@users.noreply.github.com>
2020-05-16 17:52:14 -0500

    displaying result dynamically in page

--- bundle.js
+++ bundle.js
@@ -4,18 +4,19 @@ let Phrase = require("sn1de-palindrome");
 function palindromeTester() {
 	let string = prompt("Please enter a string for palindrome testing:");
 	let phrase = new Phrase(string);
+	let palindromeResult = document.querySelector("#palindromeResult");
 
 	if (phrase.palindrome()) {
-		alert(`"${phrase.content}" is a palindrome!`);
+		palindromeResult.innerHTML = `"${phrase.content}" is a palindrome!`;
 	} else {
-		alert(`"${phrase.content}" is not a palindrome.`);
+		palindromeResult.innerHTML = `"${phrase.content}" is not a palindrome.`;
 	}
 }
 
 
 document.addEventListener("DOMContentLoaded", function() {
 	let form = document.querySelector("#palindromeTester");
-	form.addEventListener("submit", function() {
+	form.addEventListener("click", function() {
 		palindromeTester();
 	});
 });
